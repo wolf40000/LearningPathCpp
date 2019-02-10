5d8157e692abfcbc966e74ccaa5cb25ca5059f1d
Bogdan Shamborsky <bug3227@gmail.com>
2019-02-10 12:42:41 +0200

    [ST Chapter 2] Added Chapter 2 with Exercise 1 and Chapter 3

---
+++ Programming: Principles and Practice Using C++/Chapter 2/Exercise 1/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * Change the program to output the two lines:
+ *
+ *  Hello, programming!
+ *  Here we go!
+ */
+
+
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Hello, programming!" << std::endl;
+    std::cout << "Here we go!" << std::endl;
+
+    return 0;
+}
